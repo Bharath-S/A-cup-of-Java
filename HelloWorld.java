903798bce203b44d5b64b6ffb23b7c75d5e5f253
Bharath <bharath.somashekar29@gmail.com>
2018-11-10 06:59:49 +0100

    Hello World, here we meet once again!

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+ *  Compilation:  javac HelloWorld.java
+ *  Execution:    java HelloWorld
+ *
+ *  Prints "Hello, World". By tradition, this is everyone's first program.
+ *
+ *  % java HelloWorld
+ *  Hello, World
+ *
+ *  Traditon!, Got to say hello to the world every time you learn a new language
+ *
+ ******************************************************************************/
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println(" Ahhhhhhh, Hello, World!!, here we meet again");
+    }
+
+}
+
